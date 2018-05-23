e6e02267c83cbd8d74ca03e4e015585ff1e701bc
Davy <davenickmalone@gmail.com>
2018-05-24 00:36:59 +0100

    implemented simple rendering pipeline

---
+++ src/com/dotzipfile/renderer3d/models/Tetrahedron.java
@@ -0,0 +1,28 @@
+package com.dotzipfile.renderer3d.models;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tetrahedron {
+	
+	public Tetrahedron() {
+		List<Triangle> tris = new ArrayList<Triangle>();
+		tris.add(new Triangle(new Vector3(100, 100, 100),
+		                      new Vector3(-100, -100, 100),
+		                      new Vector3(-100, 100, -100),
+		                      Color.WHITE));
+		tris.add(new Triangle(new Vector3(100, 100, 100),
+		                      new Vector3(-100, -100, 100),
+		                      new Vector3(100, -100, -100),
+		                      Color.RED));
+		tris.add(new Triangle(new Vector3(-100, 100, -100),
+		                      new Vector3(100, -100, -100),
+		                      new Vector3(100, 100, 100),
+		                      Color.GREEN));
+		tris.add(new Triangle(new Vector3(-100, 100, -100),
+		                      new Vector3(100, -100, -100),
+		                      new Vector3(-100, -100, 100),
+		                      Color.BLUE));
+	}
+}
---
+++ src/com/dotzipfile/renderer3d/models/Triangle.java
@@ -0,0 +1,17 @@
+package com.dotzipfile.renderer3d.models;
+
+import java.awt.Color;
+
+public class Triangle {
+	public Vector3 v1;
+    public Vector3 v2;
+    public Vector3 v3;
+    public Color color;
+
+    public Triangle(Vector3 v1, Vector3 v2, Vector3 v3, Color color) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.color = color;
+    }
+}
